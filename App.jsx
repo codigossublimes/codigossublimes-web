4068251a3aa48fc833557665beb40f7d6b37ab73
codigossublimes <codigosdelarealidad@gmail.com>
2026-07-05 11:35:52 -0500

    Update App.jsx
    Edición del código

--- App.jsx
+++ App.jsx
@@ -3,7 +3,6 @@ import { QueryClientProvider } from '@tanstack/react-query'
 import { queryClientInstance } from '@/lib/query-client'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { AuthProvider } from '@/lib/AuthContext';
-import ScrollToTop from './components/ScrollToTop';
 import WebsitePreview from './pages/WebsitePreview';
 
 function App() {
@@ -11,7 +10,6 @@ function App() {
     <AuthProvider>
       <QueryClientProvider client={queryClientInstance}>
         <Router>
-          <ScrollToTop />
           <Routes>
             <Route path="/" element={<WebsitePreview />} />
             <Route path="*" element={<WebsitePreview />} />
